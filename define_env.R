0430a2280ca84fc67276d40a9922317cc41431bf
Daniel K Feinberg <63426144+danielkfeinberg@users.noreply.github.com>
2023-11-26 20:59:08 +0000

    Final fix for default.nix

--- define_env.R
+++ define_env.R
@@ -1,24 +1,32 @@
 library(rix)
 
-rix(r_ver = "4.3.1",
-    r_pkgs = c("quarto", "radian", "curl", "openssl", "tidyverse"),
-    system_pkgs = "quarto", "curl", "libcurl4-openssl-dev", "libssl-dev"),
-    git_pkgs = list(package_name = "rix",
+rix::rix(
+  r_ver = "4.2.3",
+  r_pkgs = c(
+    "quarto", 
+    "tidyverse"
+  ),
+  system_pkgs = c(
+    "quarto", 
+    "curl", 
+    "openssl"
+  ),
+  git_pkgs = list(package_name = "rix",
                    repo_url = "https://github.com/b-rodrigues/rix/",
                    branch_name = "master",
                    commit = "ea92a88ecdfc2d74bdf1dde3e441d008521b1756"),
-    tex_pkgs = c(
-      "amsmath",
-      "framed",
-      "fvextra",
-      "environ",
-      "fontawesome5",
-      "orcidlink",
-      "pdfcol",
-      "tcolorbox",
-      "tikzfill"
+  tex_pkgs = c(
+    "amsmath",
+    "framed",
+    "fvextra",
+    "environ",
+    "fontawesome5",
+    "orcidlink",
+    "pdfcol",
+    "tcolorbox",
+    "tikzfill"
     ),
-    ide = "rstudio",
+    ide = "code",
     shell_hook = "",
     project_path = ".",
     overwrite = TRUE,
